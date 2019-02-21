8672750c9780c3d6cb84425c791499889ed987e7
cheelou <cheetahjerry@gmail.com>
2019-02-21 21:01:49 +0800

    QuickSort

---
+++ src/main/java/demo/QuickSort.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class QuickSort {
+
+    public static <T>T[] sort(T[] array){
+        return null;
+    }
+
+    public static int pivot(){
+        return 0;
+    }
+
+    public static void main(String[]args){
+        Integer[] arr = {1,3,48,34,6,8,55,33,111,346,234,156,7,66,99,32};
+        System.out.println(arr);
+        sort(arr);
+    }
+}
