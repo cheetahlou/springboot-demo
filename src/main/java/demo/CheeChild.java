40fe4f5f9d8f5054d9fa768b800703bbc2e1e2de
cheelou <cheetahjerry@gmail.com>
2019-03-26 11:43:32 +0800

    SYSTEM separator and current directory

--- src/main/java/demo/CheeChild.java
+++ src/main/java/demo/CheeChild.java
@@ -6,6 +6,11 @@ package demo;
 public class CheeChild extends Chee {
     public static void main(String[] args) {
         CheeChild chil = new CheeChild();
-        System.out.println(chil.getZzz());//子类继承父类后属性值也继承
+        System.out.print(chil.getZzz());//子类继承父类后属性值也继承
+        String property = System.getProperty("line.separator");//不同系统换行符
+        System.out.print("111:");
+        System.out.print(property);
+        String curDir = System.getProperty("user.dir");//当前工作目录
+        System.out.println(curDir);
     }
 }
