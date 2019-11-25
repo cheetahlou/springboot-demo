387f2bf78a25dc6ea4cb6d201f463fb356902c01
louzf <1092267135@qq.com>
2019-11-25 17:35:06 +0800

    线程状态 piped 通信 wait/notify

---
+++ base/src/main/java/com/cheelou/base/threadpool/SleepUtils.java
@@ -0,0 +1,18 @@
+package com.cheelou.base.threadpool;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author louzf
+ * @decription
+ * @createTime 2019/11/22 13:43
+ */
+
+public class SleepUtils {
+    public static final void second(long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+        }
+    }
+}
